e3a6f4c813f1c0f649c2de9d42c9557fdf6b2593
Lam Nguyen <lamtnguyen989@gmail.com>
2025-05-27 13:35:36 -0500

    compile and ready to push executable

---
+++ build/CMakeFiles/inviscid-burgers.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/overlord_max/summer-2025-research/server-files/inviscid-burgers.cc" "CMakeFiles/inviscid-burgers.dir/inviscid-burgers.cc.o" "gcc" "CMakeFiles/inviscid-burgers.dir/inviscid-burgers.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/inviscid-burgers.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inviscid-burgers.dir/inviscid-burgers.cc.o"
+  "CMakeFiles/inviscid-burgers.dir/inviscid-burgers.cc.o.d"
+  "inviscid-burgers"
+  "inviscid-burgers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inviscid-burgers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
